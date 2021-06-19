6774b4b05a01f569ef588b9a9452cb2298d90d0f
Arthur Cisotto <80589710+ArthurCisotto@users.noreply.github.com>
2021-06-19 18:43:12 -0300

    Create celsius_para_fahrenheit.py

---
+++ Aula 02. Função/celsius_para_fahrenheit.py
@@ -0,0 +1,6 @@
+##Função recebe temperatura em celsius como argumento
+def celsius_para_fahrenheit(temperatura_celsius):
+    temperatura_fahrenheit = 1.8 * temperatura_celsius + 32
+    ##Conversão para Fahrenheit (C/5 = F-32/9)
+    return temperatura_fahrenheit
+    ##Retorna a temperatura convertida em fahrenheits
