52dcb8c35c6a8921a56a0ee91cc89f60437fc3b5
Arthur Cisotto <80589710+ArthurCisotto@users.noreply.github.com>
2021-06-19 20:15:51 -0300

    Create calcula_soma.py

---
+++ Aula 05. Laço/calcula_soma.py
@@ -0,0 +1,6 @@
+soma = 0
+num = float(input('Digite o número que você deseja somar ou digite 0 para concluir a soma'))
+while num != 0:
+    soma = soma + num
+    num = float(input('Digite o número que você deseja somar ou digite 0 para concluir a soma'))
+print('A soma é:', soma)
