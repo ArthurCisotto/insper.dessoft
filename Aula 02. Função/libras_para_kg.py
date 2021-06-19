9c82640512dafd85e5925d1d3ba4da60966d6b16
Arthur Cisotto <80589710+ArthurCisotto@users.noreply.github.com>
2021-06-19 18:43:33 -0300

    Create libras_para_kg.py

---
+++ Aula 02. Função/libras_para_kg.py
@@ -0,0 +1,6 @@
+##Função recebe peso em libras como argumento
+def libras_para_kg(peso_lb):
+     ##Conversão para quilogramas com a constante do enunciado
+    peso_kg = peso_lb * 0.45359237
+    return peso_kg
+    ##Retorna o peso convertido em quilogramas
